3c5ff0ef83effca6440956c46549372e3f6b867d
aungaung-frobom <aung.aung@frobom.com>
2017-04-27 09:31:28 +0000

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -17,16 +17,7 @@ module.exports = function(sequelize, Sequelize) {
             type: Sequelize.STRING,
             notEmpty: true
         },
- 
-        username: {
-            type: Sequelize.TEXT
-        },
- 
-        about: {
-            type: Sequelize.TEXT
-        },
- 
-        email: {
+     email: {
             type: Sequelize.STRING,
             validate: {
                 isEmail: true
@@ -37,11 +28,6 @@ module.exports = function(sequelize, Sequelize) {
             type: Sequelize.STRING,
             allowNull: false
         },
- 
-        last_login: {
-            type: Sequelize.DATE
-        },
- 
         status: {
             type: Sequelize.ENUM('active', 'inactive'),
             defaultValue: 'active'
